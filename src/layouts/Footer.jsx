db58291a86a5d895b2dfbf3bc9e7076a4f88db8c
AgPanoramDigital <sitespanoram2@gmail.com>
2025-02-03 11:52:21 -0300

    footer video added

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from "react";
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  const container = useRef(null);
+  const stickyMask = useRef(null);
+
+  const initialMaskSize = 0.8;
+
+  const targetMaskSize = 80;
+
+  useEffect(() => {
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    const maskSizeProgress = targetMaskSize * getScrollProgress();
+
+    stickyMask.current.style.webkitMaskSize =
+      (initialMaskSize + maskSizeProgress) * 100 + "%";
+
+    requestAnimationFrame(animate);
+  };
+
+  const getScrollProgress = () => {
+    const scrollProgress =
+      stickyMask.current.offsetTop /
+      (container.current.getBoundingClientRect().height - window.innerHeight);
+
+    return scrollProgress;
+  };
+
+  return (
+    <main className={styles.main}>
+      <div ref={container} className={styles.container}>
+        <div className={styles.stickyMask} ref={stickyMask}>
+          <video autoPlay muted loop>
+            <source
+              src="/images/videos/footer-background.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Footer;
